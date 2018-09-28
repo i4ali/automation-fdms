af470ad2be877dc6dfef0afe1fce60c12c86f3b6
i4ali999 <imran.ali@wdvglab.com>
2018-09-28 17:04:46 -0500

    added ddt

--- automation-UI/tests/landing/tests_landing.py
+++ automation-UI/tests/landing/tests_landing.py
@@ -1,28 +1,43 @@
 from pages.landing.landing_page import LandingPage
 import unittest
+from ddt import ddt, data, unpack
+from utilities.read_data import getCSVData
+from pymongo import MongoClient
 
-
-
+@ddt
 class LandingPageTest(unittest.TestCase):
 
-    def setUpClass(cls):
-        pass
+    def setUp(self):
+        conn = MongoClient("mongodb://localhost:31001/")
+        db = conn['service-fdms']
+        well = db.get_collection('well')
+        well.delete_many({})
 
     def test_can_go_to_landing_page(self):
         self.lp = LandingPage("firefox")
         self.lp.goto()
         assert self.lp.isat()
 
-    def test_add_new_well(self):
+    @data(*getCSVData('testdata/welltestdata.csv'))
+    @unpack
+    def test_add_new_well(self, wellname, apinumber):
         self.lp = LandingPage("firefox")
         self.lp.goto()
-        self.lp.addnewwell('test', '12')
+        self.lp.addnewwell(wellname, apinumber)
         assert self.lp.wellsuccessmessagepops()
-        assert self.lp.wellexists('test')
+        assert self.lp.wellexists(wellname)
 
-    def tearDownClass(cls):
-        pass
-
+    # @data(*getCSVData('../../testdata/welltestdata.csv'))
+    # @unpack
+    # @unittest.skip("WIP")
+    # def test_rem_well(self, wellname, apinumber):
+    #     self.lp = LandingPage("firefox")
+    #     self.lp.goto()
+    #     self.lp.addnewwell(wellname, apinumber)
+    #     assert self.lp.wellsuccessmessagepops()
+    #     assert self.lp.wellexists(wellname)
+    #     self.lp.removewell(wellname)
+    #     assert self.lp.welldoesntexist(wellname)
 
 
 if __name__ == '__main__':
--- automation-UI/tests/project/tests_project.py
+++ automation-UI/tests/project/tests_project.py
@@ -1,8 +1,19 @@
 from pages.projects.project_page import ProjectPage
 import unittest
+from pymongo import MongoClient
+from utilities.read_data import getCSVData
+from ddt import ddt, data, unpack
 
-
+@ddt
 class ProjectPageTest(unittest.TestCase):
+
+    def setUp(self):
+        conn = MongoClient("mongodb://localhost:31001/")
+        db = conn['service-fdms']
+        project = db.get_collection('project')
+        client = db.get_collection('client')
+        project.delete_many({})
+        client.delete_many({})
 
 
     def test_can_go_to_project_page(self):
@@ -10,9 +21,11 @@ class ProjectPageTest(unittest.TestCase):
         self.pp.goto()
         assert self.pp.isat()
 
-    def test_add_new_project(self):
+    @data(*getCSVData('testdata/projecttestdata.csv'))
+    @unpack
+    def test_add_new_project(self, Projectname, Companyname, Wellname, APInumber):
         self.pp = ProjectPage("firefox")
         self.pp.goto()
-        self.pp.addnewproject('test8', 'test8', 'test8')
+        self.pp.addnewproject(Projectname, Companyname, Wellname, APInumber)
         assert self.pp.projectsuccessmessagepops()
-        assert self.pp.projectexists('test8')
+        assert self.pp.projectexists(Projectname)
